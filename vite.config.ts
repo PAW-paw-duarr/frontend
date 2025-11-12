daaf57f2c20307eaf19209a652921842fdbdd096
hilmoo <hilmusyaf2005@gmail.com>
2025-11-12 08:34:17 +0700

    hotfix: hmr not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
@@ -15,7 +16,8 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     tsconfigPaths(),
-    tailwindcss()
+    tailwindcss(),
+    react()
   ],
 
 });
